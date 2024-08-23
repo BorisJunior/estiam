490447ca2cdb9592bbb71f3becdfef864bbe4836
Boris Junior <60442835+BorisJunior@users.noreply.github.com>
2024-08-23 22:22:24 +0200

    Update app.py/done

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for, session
 import sqlite3
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
-
 
 # Page d'accueil utilisateur avec le formulaire de contact
 @app.route("/", methods=['GET', 'POST'])
